83210df37439f190bcfd8ef38ff75697763dbdf3
cdvoracek <chad.dvoracek@gmail.com>
2013-02-23 16:53:21 -0600

    Commit_new

---
+++ SEIS610_asgn/Dvoracek_Asgn_01/src/dvoracek_asgn_01/Dvoracek_Asgn_01.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dvoracek_asgn_01;
+
+/**
+ *SEIS 610: Software Engineering
+ * @author Chad Dvoracek
+ * Assignment #1
+ * Due Date: March 2nd
+ * 
+ */
+
+
+public class Dvoracek_Asgn_01 {
+
+     /**
+     * @param args the command line arguments
+     * Target Function(Fitness Function): Y = (5x^2-5)/4
+     *
+     * Random Functions are:  Y=(2x^2+2)/6 and Y=(8x^2-9)/9 
+     * 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // assign variables
+     int x=2;
+     double random1,exp1, random2, exp2;
+     //double random2= (8x^2-9)/9;
+     
+     random
+     
+     //assign exmponent calculations
+        exp1 = 2*(Math.pow(4,2));
+     
+     System.out.println ("this is the value of random1 "+exp1);
+    }
+}
