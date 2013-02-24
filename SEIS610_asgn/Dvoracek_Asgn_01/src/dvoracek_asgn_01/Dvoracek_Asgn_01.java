9b39772a838e977a9d72cb692de99de50b938e4e
cdvoracek <chad.dvoracek@gmail.com>
2013-02-24 10:14:07 -0600

    Com8
    Still working on array.

--- SEIS610_asgn/Dvoracek_Asgn_01/src/dvoracek_asgn_01/Dvoracek_Asgn_01.java
+++ SEIS610_asgn/Dvoracek_Asgn_01/src/dvoracek_asgn_01/Dvoracek_Asgn_01.java
@@ -22,7 +22,7 @@ public class Dvoracek_Asgn_01 {
     public static void main(String[] args) {
     // TODO code application logic here
     // assign variables 
-     int n,x, r1score, r2score, beginx, lastx;
+     int n,x,nx, r1score, r2score, beginx, lastx, arraysize;
      String testset1, testset2, t1,r1, r2;
      double target,random1,random2,test1,test2 ;
      
@@ -30,15 +30,22 @@ public class Dvoracek_Asgn_01 {
      r1score=0;
      r2score=0;
      x=-25;
+     nx=25;
      beginx=x;
-     lastx=0;
      t1="Y=(5x^2-5)/4";
      r1="Y=(2x^2+2)/6";
      r2="Y=(8x^2-9)/9";
+     arraysize=(Math.abs (beginx-nx))+1;
      
+     //JOptionPane.showMessageDialog (null,"Array size = "+(arraysize));
+     //name output arrays
+    String [] TestSetOutput1 = new String[arraysize]; 
+    String [] TestSetOutput2 = new String[arraysize];  
+  
+     //DecimalFormat fmt= new DecimalFormat ("0.###"); 
      //Loop statement to test values.
      
-     while (x<=25)
+     while (x<=nx)
      { 
      target= (5*(Math.pow(x,2))-5)/4;
      random1= (2*(Math.pow(x,2))+2)/6;
@@ -76,16 +83,14 @@ public class Dvoracek_Asgn_01 {
  
      //Capturing Test Set Data 
      
-    String [] TestSetOutput1 = new String[(Math.abs (beginx-lastx))]; 
-    String [] TestSetOutput2 = new String[(Math.abs (beginx-lastx))];  
-  
+    
     
     //Test output of data sets
     
     //System.out.print("Random 1: X"+(x+1)+"="+x+" ,Y"+x+"="+random1);
     x++;
     //System.out.print ("\n"); 
-    lastx=(x-1); 
+   
      
      
      }
@@ -94,16 +99,16 @@ public class Dvoracek_Asgn_01 {
         System.out.println ("Target fuction "+t1); 
         System.out.println("Random 1 ("+r1+")score="+r1score);
         System.out.println("Random 2 ("+r2+")score="+r2score);
-        System.out.println("X was tested from "+beginx+" to "+lastx);
-        JOptionPane.showMessageDialog (null,"X was tested from "+beginx+" to "+lastx+".\nRandom 1 "+r1+" wins "+r1score+" to "+r2score+".\nIt is closer to the target function of \n"+t1);
+        System.out.println("X was tested from "+beginx+" to "+nx);
+        JOptionPane.showMessageDialog (null,"X was tested from "+beginx+" to "+nx+".\nRandom 1 "+r1+" wins "+r1score+" to "+r2score+".\nIt is closer to the target function of \n"+t1);
         
             }
      else {
         System.out.println ("Target fuction "+t1); 
         System.out.println("Random 1 ("+r1+")score="+r1score);
         System.out.println("Random 2 ("+r2+")score="+r2score);
-        System.out.println("X was tested from "+beginx+" to "+lastx);
-        JOptionPane.showMessageDialog (null,"X was tested from "+beginx+" to "+lastx+".\nRandom 2 "+r2+" wins "+r2score+" to "+r1score+".\nIt is closer to the target function of \n"+t1);
+        System.out.println("X was tested from "+beginx+" to "+nx);
+        JOptionPane.showMessageDialog (null,"X was tested from "+beginx+" to "+nx+".\nRandom 2 "+r2+" wins "+r2score+" to "+r1score+".\nIt is closer to the target function of \n"+t1);
             }
     
      System.out.println ("The End");
